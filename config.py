b5a8f593c7be2ccbcff057171e82f7abaa44fbe0
AkatsukiDeki <124888750+AkatsukiDeki@users.noreply.github.com>
2024-07-06 00:10:10 +0400

    First tgBot

---
+++ config.py
@@ -0,0 +1,37 @@
+
+TOKEN = '6773971928:AAHOnRSf3iTiqC94tL4dgxNlpdTNVVRTex4'
+
+
+keys = {
+  "bitcoin": "BTC",
+  "Bitcoin": "BTC",
+  "BTC": "BTC",
+  "биткоин": "BTC",
+  "Биткоин": "BTC",
+  "Бтс": "BTC",
+  "БТС": "BTC",
+  "бтс": "BTC",
+  "ethereum": "ETH",
+  "Ethereum": "ETH",
+  "ETH": "ETH",
+  "Эфириум": "ETH",
+  "эфириум": "ETH",
+  "eth": "ETH",
+  "dollar": "USD",
+  "Dollar": "USD",
+  "Доллар": "USD",
+  "доллар": "USD",
+  "$": "USD",
+  "USD": "USD",
+  "euro": "EUR",
+  "Euro": "EUR",
+  "Евро": "EUR",
+  "евро": "EUR",
+  "€": "EUR",
+  "EUR": "EUR",
+  "Ruble": "RUB",
+  "ruble": "RUB",
+  "Рубль": "RUB",
+  "рубль": "RUB",
+  "₽": "RUB"
+}
